238aeec05a6bdb9e2740683ccfd7134565276210
Qoosim <ayinde.abdulghaniyy@gmail.com>
2022-02-10 16:59:13 +0100

    Implement the logics that make the calculator work

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,36 +1,155 @@
 import React from 'react';
+import calculate from '../logic/calculate';
 
 class Calculator extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
+
+    this.state = {
+      total: '0',
+      next: null,
+      operation: null,
+    };
+
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick(event) {
+    this.setState((state) => calculate(state, event.target.textContent));
   }
 
   render() {
+    const { total, next, operation } = this.state;
+
     return (
       <div className="calc-grid">
         <div className="output-div">
-          <div className="output">0</div>
+          <div className="output">
+            { total || ''}
+            { operation || ''}
+            { next || ''}
+          </div>
         </div>
-        <button type="button">AC</button>
-        <button type="button">+/-</button>
-        <button type="button">%</button>
-        <button type="button" className="operation">÷</button>
-        <button type="button">7</button>
-        <button type="button">8</button>
-        <button type="button">9</button>
-        <button type="button" className="operation">×</button>
-        <button type="button">4</button>
-        <button type="button">5</button>
-        <button type="button">6</button>
-        <button type="button" className="operation">-</button>
-        <button type="button">1</button>
-        <button type="button">2</button>
-        <button type="button">3</button>
-        <button type="button" className="operation">+</button>
-        <button type="button" className="span-two">0</button>
-        <button type="button">.</button>
-        <button type="button" className="operation">=</button>
+        <button
+          type="button"
+          onClick={(event) => this.handleClick(event)}
+        >
+          AC
+        </button>
+        <button
+          type="button"
+          onClick={(event) => this.handleClick(event)}
+        >
+          +/-
+        </button>
+        <button
+          type="button"
+          onClick={(event) => this.handleClick(event)}
+        >
+          %
+        </button>
+        <button
+          type="button"
+          onClick={(event) => this.handleClick(event)}
+          className="operation"
+        >
+          ÷
+        </button>
+        <button
+          type="button"
+          onClick={(event) => this.handleClick(event)}
+        >
+          7
+        </button>
+        <button
+          type="button"
+          onClick={(event) => this.handleClick(event)}
+        >
+          8
+        </button>
+        <button
+          type="button"
+          onClick={(event) => this.handleClick(event)}
+        >
+          9
+        </button>
+        <button
+          type="button"
+          onClick={(event) => this.handleClick(event)}
+          className="operation"
+        >
+          x
+        </button>
+        <button
+          type="button"
+          onClick={(event) => this.handleClick(event)}
+        >
+          4
+        </button>
+        <button
+          type="button"
+          onClick={(event) => this.handleClick(event)}
+        >
+          5
+        </button>
+        <button
+          type="button"
+          onClick={(event) => this.handleClick(event)}
+        >
+          6
+        </button>
+        <button
+          type="button"
+          onClick={(event) => this.handleClick(event)}
+          className="operation"
+        >
+          -
+        </button>
+        <button
+          type="button"
+          onClick={(event) => this.handleClick(event)}
+        >
+          1
+        </button>
+        <button
+          type="button"
+          onClick={(event) => this.handleClick(event)}
+        >
+          2
+        </button>
+        <button
+          type="button"
+          onClick={(event) => this.handleClick(event)}
+        >
+          3
+        </button>
+        <button
+          type="button"
+          onClick={(event) => this.handleClick(event)}
+          className="operation"
+        >
+          +
+        </button>
+        <button
+          type="button"
+          onClick={(event) => this.handleClick(event)}
+          className="span-two"
+        >
+          0
+        </button>
+        <button
+          type="button"
+          onClick={(event) => this.handleClick(event)}
+        >
+          .
+        </button>
+        <button
+          type="button"
+          onClick={(event) => this.handleClick(event)}
+          className="operation"
+        >
+          =
+        </button>
       </div>
     );
   }
