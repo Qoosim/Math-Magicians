6ad29b588f1a33d6794cda7441a8a81982668f0b
Qoosim <ayinde.abdulghaniyy@gmail.com>
2022-02-14 17:19:43 +0100

    Make the calculator a full website with two other components and with their respective navigations

---
+++ src/components/Quote.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Quote = () => (
+  <main>
+    <section>
+      <h1 className="quote">Qoute of the day</h1>
+      <p className="quote-msg">
+        Mathematics is not about numbers, equations, computations or algorithm:
+        It is about understanding.
+        <br />
+        **-William Paul Thurston-**
+      </p>
+    </section>
+  </main>
+);
+
+export default Quote;
