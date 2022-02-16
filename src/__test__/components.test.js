0f34453735b28aa3bd593459468ce90625bec3d9
ixboy <ismaelixboy@gmail.com>
2022-02-17 01:46:37 +0300

    add snapshot test for calculator component

--- src/__test__/components.test.js
+++ src/__test__/components.test.js
@@ -1,8 +1,8 @@
-import React from 'react';
 import '@testing-library/jest-dom';
-import { render, fireEvent, screen } from '@testing-library/react';
-import Home from '../components/Home';
+import { fireEvent, render, screen } from '@testing-library/react';
+import React from 'react';
 import Calculator from '../components/Calculator';
+import Home from '../components/Home';
 import Quotes from '../components/Quote';
 
 describe('Tests for components', () => {
@@ -19,5 +19,16 @@ describe('Tests for components', () => {
   test('Quotes snapshot', () => {
     const tree = render(<Quotes />);
     expect(tree).toMatchSnapshot();
+  });
+
+  test('Calculator component test', () => {
+    const tree = render(<Calculator />);
+    expect(tree).toMatchSnapshot();
+    fireEvent.click(screen.getByText('2'));
+    expect(screen.getByTestId('output')).toHaveTextContent('2');
+    fireEvent.click(screen.getByText('+'));
+    fireEvent.click(screen.getByText('7'));
+    fireEvent.click(screen.getByText('='));
+    expect(screen.getByTestId('output')).toHaveTextContent('9');
   });
 });
